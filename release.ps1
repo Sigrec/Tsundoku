fd56f02d8f77e45ffed79f540cfcf77b069f271d
Sigrec <seanisaacnjenga@gmail.com>
2023-10-19 11:40:04 -0700

    Release Script Fix

--- release.ps1
+++ release.ps1
@@ -30,6 +30,7 @@ Write-Output "Version: $version"
 # Clean output directory.
 $publishDir = "bin/publish"
 $outDir = "$projDir/$publishDir"
+Write-Output "../$outDir"
 Write-Output "./$outDir"
 Write-Output "/$outDir"
 Write-Output "$outDir"
@@ -85,7 +86,7 @@ try {
 
     # Copy new application files.
     Write-Output "Copying new files..."
-    Copy-Item -Path "./$outDir/Application Files","./$outDir/$appName.application" `
+    Copy-Item -Path "../$outDir/Application Files","../$outDir/$appName.application" `
         -Destination . -Recurse
 
     # Stage and commit.
