51ec7f164d587a356b48759c3e93106198a1369e
Sigrec <seanisaacnjenga@gmail.com>
2026-03-31 18:02:29 -0500

    feat: add version navigation arrows to changelog window
    Add previous/next buttons to browse changelogs from older versions, add 2.0.1 changelog entry, and clarify the "Refresh Covers on Startup" setting label.

--- Src/Views/ChangelogWindow.axaml.cs
+++ Src/Views/ChangelogWindow.axaml.cs
@@ -8,6 +8,9 @@ namespace Tsundoku.Views;
 
 public sealed partial class ChangelogWindow : ReactiveWindow<ViewModelBase>
 {
+    private string[] _sortedVersions = [];
+    private int _currentIndex;
+
     public ChangelogWindow()
     {
         InitializeComponent();
@@ -15,11 +18,27 @@ public sealed partial class ChangelogWindow : ReactiveWindow<ViewModelBase>
         ChangesScroll.GetObservable(ScrollViewer.ExtentProperty).Subscribe(_ => UpdateScrollHint());
         ChangesScroll.GetObservable(ScrollViewer.ViewportProperty).Subscribe(_ => UpdateScrollHint());
         ChangesScroll.GetObservable(ScrollViewer.OffsetProperty).Subscribe(_ => UpdateScrollHint());
+
+        _sortedVersions = Changelog.Entries.Keys
+            .OrderBy(v => Version.TryParse(v, out Version? parsed) ? parsed : new Version(0, 0))
+            .ToArray();
     }
 
     public void SetVersion(string version)
     {
+        _currentIndex = Array.IndexOf(_sortedVersions, version);
+        if (_currentIndex < 0)
+        {
+            _currentIndex = _sortedVersions.Length - 1;
+        }
+        ShowVersion(_sortedVersions[_currentIndex]);
+    }
+
+    private void ShowVersion(string version)
+    {
         VersionHeader.Text = $"What's New in v{version}";
+        ChangesScroll.Offset = default;
+
         if (Changelog.Entries.TryGetValue(version, out ChangelogEntry? entry))
         {
             ChangesItems.ItemsSource = entry.Changes;
@@ -29,6 +48,45 @@ public sealed partial class ChangelogWindow : ReactiveWindow<ViewModelBase>
                 ActionsItems.ItemsSource = entry.Actions;
                 ActionsSection.IsVisible = true;
             }
+            else
+            {
+                ActionsItems.ItemsSource = null;
+                ActionsSection.IsVisible = false;
+            }
+        }
+        else
+        {
+            ChangesItems.ItemsSource = null;
+            ActionsItems.ItemsSource = null;
+            ActionsSection.IsVisible = false;
+        }
+
+        bool hasPrev = _currentIndex > 0;
+        PrevVersionButton.Opacity = hasPrev ? 1 : 0;
+        PrevVersionButton.IsHitTestVisible = hasPrev;
+        ToolTip.SetTip(PrevVersionButton, hasPrev ? $"v{_sortedVersions[_currentIndex - 1]}" : null);
+
+        bool hasNext = _currentIndex < _sortedVersions.Length - 1;
+        NextVersionButton.Opacity = hasNext ? 1 : 0;
+        NextVersionButton.IsHitTestVisible = hasNext;
+        ToolTip.SetTip(NextVersionButton, hasNext ? $"v{_sortedVersions[_currentIndex + 1]}" : null);
+    }
+
+    private void OnPrevVersionClicked(object? sender, RoutedEventArgs e)
+    {
+        if (_currentIndex > 0)
+        {
+            _currentIndex--;
+            ShowVersion(_sortedVersions[_currentIndex]);
+        }
+    }
+
+    private void OnNextVersionClicked(object? sender, RoutedEventArgs e)
+    {
+        if (_currentIndex < _sortedVersions.Length - 1)
+        {
+            _currentIndex++;
+            ShowVersion(_sortedVersions[_currentIndex]);
         }
     }
 
